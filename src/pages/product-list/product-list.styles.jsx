e843db134981878f53b7656e8c2480027a459d49
Cristiandgr <cristiandgr24@gmail.com>
2023-02-16 20:38:53 +0000

    fix footer

--- src/pages/product-list/product-list.styles.jsx
+++ src/pages/product-list/product-list.styles.jsx
@@ -1,9 +1,9 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const DashboardContainer = styled.div`
   width: 100%;
   height: 100%;
-`;
+`
 
 export const ProductListContainer = styled.div`
   display: grid;
@@ -11,6 +11,7 @@ export const ProductListContainer = styled.div`
   grid-gap: 20px;
   margin: 20px 40px;
   justify-items: center;
+  min-height: 66vh;
 
   @media screen and (max-width: 768px) {
     grid-template-columns: repeat(2, 1fr);
@@ -19,11 +20,11 @@ export const ProductListContainer = styled.div`
   @media screen and (max-width: 380px) {
     grid-template-columns: repeat(1, 1fr);
   }
-`;
+`
 
 export const EmptyProducts = styled.p`
   font-size: 32px;
   font-weight: 600;
   position: absolute;
   text-align: center;
-`;
+`
